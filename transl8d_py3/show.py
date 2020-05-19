b29d86e9d8f637f97e0cf6837bec20f6d56d4217
neonb88 <nathanbendich@gmail.com>
2020-05-19 13:59:07 -0400

    show.py : Tue May 19 13:59:06 EDT 2020

--- transl8d_py3/show.py
+++ transl8d_py3/show.py
@@ -15,25 +15,95 @@ import numpy as np
 import skimage.io as io
 import tensorflow as tf
 
-from hmr.src.util import renderer as vis_util
-from hmr.src.util import image as img_util
-from hmr.src.util import openpose as op_util
-import hmr.src.config
-from hmr.src.RunModel import RunModel
-
-from hmr.demo import visualize
+from src.util import renderer as vis_util
+from src.util import image as img_util
+from src.util import openpose as op_util
+import src.config
+from src.RunModel import RunModel
 
 import imageio as ii
 
+from cx import parse_obj_file
+
+flags.DEFINE_string('img_path', 'hmr/data/im1963.jpg', 'Image to run')
+flags.DEFINE_string(
+    'json_path', None,
+    'If specified, uses the openpose output to crop the image.')
+
+def visualize(img, proc_param, joints, verts, cam):
+    """
+    Renders the result in original image coordinate frame.
+    """
+    cam_for_render, vert_shifted, joints_orig = vis_util.get_original(
+        proc_param, verts, cam, joints, img_size=img.shape[:2])
+
+    # Render results
+    skel_img = vis_util.draw_skeleton(img, joints_orig)
+    rend_img_overlay = renderer(
+        vert_shifted, cam=cam_for_render, img=img, do_alpha=True)
+    rend_img = renderer(
+        vert_shifted, cam=cam_for_render, img_size=img.shape[:2])
+    rend_img_vp1 = renderer.rotated(
+        vert_shifted, 60, cam=cam_for_render, img_size=img.shape[:2])
+    rend_img_vp2 = renderer.rotated(
+        vert_shifted, -60, cam=cam_for_render, img_size=img.shape[:2])
+
+    import matplotlib.pyplot as plt
+    # plt.ion()
+    plt.figure(1)
+    plt.clf()
+    plt.subplot(231)
+    plt.imshow(img)
+    plt.title('input')
+    plt.axis('off')
+    plt.subplot(232)
+    plt.imshow(skel_img)
+    plt.title('joint projection')
+    plt.axis('off')
+    plt.subplot(233)
+    plt.imshow(rend_img_overlay)
+    plt.title('3D Mesh overlay')
+    plt.axis('off')
+    plt.subplot(234)
+    plt.imshow(rend_img)
+    plt.title('3D mesh')
+    plt.axis('off')
+    plt.subplot(235)
+    plt.imshow(rend_img_vp1)
+    plt.title('diff vp')
+    plt.axis('off')
+    plt.subplot(236)
+    plt.imshow(rend_img_vp2)
+    plt.title('diff vp')
+    plt.axis('off')
+    plt.draw()
+    plt.show()
+    # import ipdb
+    # ipdb.set_trace()
+
 if __name__=="__main__":
   config = flags.FLAGS
-  renderer = vis_util.SMPLRenderer(face_path=config.smpl_face_path)
-  img_path='/home/nathanbendich/intermediate_steps_ClothX/images/img0.jpg' # TODO:  I think we'll need to change this
+  config(sys.argv)
+  # Using pre-trained model, change this to use your own.
+  config.load_path = src.config.PRETRAINED_MODEL
+  config.batch_size = 1
 
-  #TODO: this is the sketchiest, shittiest way to get this done.  But at least we should have SOMETHING to mess with
+  renderer = vis_util.SMPLRenderer(face_path=config.smpl_face_path)
+  #img_path='/home/nathanbendich/CIHP_PGN/datasets/CIHP/images/img0.jpg'
+  img_path='/home/nathanbendich/CIHP_PGN/datasets/CIHP/images_May_19_2020_____11:40_P.M._EDT/img0.jpg'
+
+  #TODO: NOTE this is the sketchiest, shittiest way to get this done.  But at least we should have SOMETHING to mess with
+  #TODO: change!
+  #TODO: change!
+  #TODO: change!
+  #TODO: change!
+  #TODO: change!
+  #TODO: NOTE this is the sketchiest, shittiest way to get this done.  But at least we should have SOMETHING to mess with
+  tmp_obj_fname='assets/MGN_obj__2020_May_19____11:39_AM__/cust.obj'
+  vs_naked, _ = parse_obj_file(tmp_obj_fname)
   visualize( 
     np.asarray(ii.imread(img_path)), # "img"
-    {'end_pt': np.array([336, 336]) # "proc_param"
+    {'end_pt': np.array([336, 336]), # "proc_param"
       'img_size': 225,
       'scale': 1.0,
       'start_pt': np.array([112,112]),
@@ -59,6 +129,10 @@ if __name__=="__main__":
        [ 126.66687775,  51.3151474 ],
        [ 133.51797485,  57.24398041],
     ]),# "joints[0]"
-    model['vertices_naked'], # from MGN.  # the real one is in "make_SMPL_mesh.py_____dev.py"
+    #model['vertices_naked'], # from MGN.  # the real one is in "make_SMPL_mesh.py_____dev.py"
+    vs_naked,
     np.array([ 1.23492968, -0.18135118,  0.35370526]) # cams[0]  (prob "camera parameters")
   )
+
+
+
